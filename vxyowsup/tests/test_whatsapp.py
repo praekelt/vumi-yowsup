ed47760617105d15b6c7c6bee92936ac89ee3a11
Anna Borysova <annasborysova@gmail.com>
2015-06-30 12:19:53 +0200

    added protocol layers and interface layer to patched stack

--- vxyowsup/tests/test_whatsapp.py
+++ vxyowsup/tests/test_whatsapp.py
@@ -7,10 +7,11 @@ from vumi.errors import ConfigError
 
 from vumi.transports.tests.helpers import TransportHelper
 
-from vxyowsup import WhatsAppTransport
-from yowsup.stacks import YowStack
+from vxyowsup import WhatsAppTransport, WhatsAppInterface
+from yowsup.stacks import YowStack, YowStackBuilder
 from yowsup.layers.logger import YowLoggerLayer
 from yowsup.layers.__init__ import YowLayer
+from yowsup.layers import YowParallelLayer
 
 
 class TestWhatsAppTransport(VumiTestCase):
@@ -37,12 +38,17 @@ class TestWhatsAppTransport(VumiTestCase):
         # YowCoderLayer decodes bytearrays into instances of ProtocolTreeNode
         # falsify this layer and all those below it into TestingLayer
 
-        new_stack = YowStack((
-            YowLoggerLayer,
-            TestingLayer
-        )[::-1])
+        new_core_layers = (YowLoggerLayer, TestingLayer)[::-1]
+
+        new_layers = new_core_layers + \
+            YowParallelLayer(
+                YowStackBuilder.getProtocolLayers(),) + (WhatsAppInterface,)
+
+        new_stack = YowStack(new_layers, reversed=False)
+
         self.stack.setCredentials(self.transport.stack_client.CREDENTIALS)
-        self.patch(self.transport.stack_client, self.transport.stack_client.stack, new_stack)
+        self.patch(
+            self.transport.stack_client, self.transport.stack_client.stack, new_stack)
 
 #    def test_handle_outbound(self):
 #    	message = self.tx_helper.make_outbound('blep', from_addr='27xxxxxxxxx', to_addr='27xxxxxxxxxxx')
