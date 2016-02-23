556ce3b143221a5c6914576abdec7cff3518f2ad
Rudi Giesler <rudi@praekelt.com>
2016-02-23 17:57:56 +0200

    Post release version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="vxyowsup",
-    version="0.1.5",
+    version="0.1.6a",
     url='http://github.com/praekelt/vumi-yowsup',
     license='BSD',
     description="A WhatsApp transport for Vumi.",
--- vxyowsup/__init__.py
+++ vxyowsup/__init__.py
@@ -1,5 +1,5 @@
 """Vumi WhatsApp transport build using Yowsup."""
 
-__version__ = "0.1.5"
+__version__ = "0.1.6a"
 
 __all__ = []
