b35d480b2005cb19e83afac475133215338629de
Rudi Giesler <rudi@praekelt.com>
2016-04-15 17:26:43 +0200

    Release 0.1.8

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="vxyowsup",
-    version="0.1.7",
+    version="0.1.8",
     url='http://github.com/praekelt/vumi-yowsup',
     license='BSD',
     description="A WhatsApp transport for Vumi.",
